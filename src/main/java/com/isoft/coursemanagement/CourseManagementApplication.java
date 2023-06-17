d0599d9afda2a8c4164002dff7f5277b0a28b46a
mramz55555 <mramz55555@gmail.com>
2023-06-17 13:58:03 +0330

    database auto population issue fixed

--- src/main/java/com/isoft/coursemanagement/CourseManagementApplication.java
+++ src/main/java/com/isoft/coursemanagement/CourseManagementApplication.java
@@ -4,7 +4,9 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import reactor.core.publisher.Mono;
 
@@ -37,7 +39,7 @@ public class CourseManagementApplication {
 
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
-//        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
         return initializer;
     }
 }
